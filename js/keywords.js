c54f3ccaf39e2e6aec362977bcbce2655cb40432
Jackson <jacksonlango@gmail.com>
2016-01-01 13:12:35 -0800

    initial commit

---
+++ js/keywords.js
@@ -0,0 +1,27 @@
+function Keywords () 
+{
+  var words = []; // array of search strings
+  var myHilitor;
+  document.addEventListener("DOMContentLoaded", function() {
+    myHilitor = new Hilitor("content");
+    // myHilitor.apply("Python");
+  }, false);
+    
+  this.toggle = function (input) 
+  {
+    var index = words.indexOf(input)
+    if (index > -1) 
+    {
+      // if already highlighted, remove
+      words.splice(index, 1);
+      document.getElementById(input + "-button").className = "";
+    } 
+    else 
+    {
+      // otherwise add
+      words.push(input);
+      document.getElementById(input + "-button").className = "button-primary";
+    }
+    myHilitor.applyArray(words);
+  };
+}
